5d4c1f3c5a3ca3f744f5bae1ace333c00a30c8dd
Matias <matiaspupirivarola@gmail.com>
2026-01-03 21:52:01 -0300

    agregando una nueva base de datos

--- src/config.js
+++ src/config.js
@@ -3,8 +3,8 @@ import { config } from "dotenv";
 config();
 
 export const PORT = process.env.PORT || 3000;
-export const DB_HOST = process.env.DB_HOST || 'mysqldb';
-export const DB_PORT = process.env.DB_PORT || 3306;
-export const DB_USER = process.env.DB_USER || "root";
-export const DB_PASSWORD = process.env.DB_PASSWORD || "1230";
-export const DB_NAME = process.env.DB_NAME || "companydb";
+export const DB_HOST = process.env.DB_HOST;
+export const DB_PORT = process.env.DB_PORT;
+export const DB_USER = process.env.DB_USER;
+export const DB_PASSWORD = process.env.DB_PASSWORD;
+export const DB_NAME = process.env.DB_NAME;
